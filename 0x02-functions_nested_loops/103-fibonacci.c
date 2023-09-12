55e671a57f9f64fd77a341cf15490f86f17faa68
KingTiMis <programminghub613@gmail.com>
2023-09-12 21:02:17 +0000

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	int a = 0;
+	int i = 0;
 	long j = 1, k = 2, sum = k;
 
 	while (k + j < 4000000)
@@ -17,7 +17,7 @@ int main(void)
 		if (k % 2 == 0)
 		sum += k;
 		j = k - j;
-		++a;
+		++i;
 	}
 	printf("%ld", sum);
 	return (0);
